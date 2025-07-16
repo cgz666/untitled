3e37aeb255c4cbb2631a3941b46a488cdb5088a8
cgz666 <275694332@qq.com>
2025-07-16 09:27:08 +0800

    更新

--- experimental_data/爬取实验数据编号.py
+++ experimental_data/爬取实验数据编号.py
@@ -1,30 +1,33 @@
 import os
 import pandas as pd
 
-# 假设你的文件位于当前目录下的'files'文件夹中
+# 文件夹路径
 folder_path = r'F:\untitled\experimental_data\xls'
 output_folder = r'F:\untitled\experimental_data\output'  # 输出文件夹路径
 
-# 获取文件夹内所有文件的名称
-file_names = os.listdir(folder_path)
+# 确保输出文件夹存在
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+# 获取文件夹内所有PDF文件的名称
+file_names = [file for file in os.listdir(folder_path) if file.endswith('.pdf')]
 
 # 存储提取出的编号
 ids = []
 
-# 遍历文件名列表，提取编号
+# 遍历文件名列表，提取前18个字符作为编号
 for file_name in file_names:
-    if file_name.endswith('.pdf'):  # 确保只处理PDF文件
-        # 提取编号，假设编号格式为HPJC-240320-898704，位于文件名开头
-        parts = file_name.split('-')
-        if len(parts) >= 3:
-            id = parts[0] + '-' + parts[1] + '-' + parts[2]
-            ids.append(id)
+    # 提取前18个字符
+    extracted_id = file_name[:18]
+    # 检查提取的编号是否包含至少两个短横线
+    if extracted_id.count('-') >= 2:
+        ids.append(extracted_id)
 
 # 创建一个DataFrame
 df = pd.DataFrame(ids, columns=['ID'])
 
 # 将DataFrame保存到CSV文件
-output_file_path = os.path.join(output_folder, 'extracted_ids.csv')  # 输出文件路径
-df.to_csv(output_file_path, index=False)  # 不保存行索引
+output_file_path = os.path.join(output_folder, 'extracted_ids.csv')
+df.to_csv(output_file_path, index=False)
 
 print(f"提取的编号已保存到 {output_file_path}")
